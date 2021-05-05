ee4b4566a388c9e95cecbc8ddc9bb13cfc859218
igotbitches <check.point9917@yandex.ru>
2021-05-05 18:07:40 +0300

    change place for code at `headers.component`

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -12,16 +12,16 @@ export class HeaderComponent implements OnInit {
   isVisible: boolean
 
   constructor(private authService: AuthService, private router: Router) {
+
+  }
+
+  ngOnInit(): void {
     this.authService.getData().subscribe(res => {
       this.isVisible = !!(res && res.uid);
       if (!this.isVisible) {
         this.router.navigate(['/login'])
       }
-  })
-  }
-
-  ngOnInit(): void {
-
+    })
   }
 
   logout() {
