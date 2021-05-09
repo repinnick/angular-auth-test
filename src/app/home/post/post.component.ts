7510e047ed9534107a354ccfb187e469208449f1
igotbitches <check.point9917@yandex.ru>
2021-05-09 21:17:08 +0300

    start to create delete function

--- src/app/home/post/post.component.ts
+++ src/app/home/post/post.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Post} from "../../shared/interfaces";
+import {PostService} from "../../shared/services/post.service";
 
 @Component({
   selector: 'app-post',
@@ -10,9 +11,14 @@ export class PostComponent implements OnInit {
 
   @Input() post: Post
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
   }
 
+  delete(id: string) {
+    this.postService.deletePost(id).subscribe(() => {
+      console.log('deleted')
+    }, error => console.log(error.message))
+  }
 }
