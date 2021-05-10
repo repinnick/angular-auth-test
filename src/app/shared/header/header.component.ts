50a6407a8d7052a27c9fb89cd0fb1384b7e446a8
igotbitches <check.point9917@yandex.ru>
2021-05-10 11:15:46 +0300

    change some functionality

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,21 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from "../services/auth.service";
 import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   isAuth: boolean;
+  isVisible: boolean;
+  authSubscription: Subscription;
 
   constructor(public authService: AuthService, private router: Router) {
   }
 
   ngOnInit(): void {
-    this.authService.getCurrentUser().subscribe(res => {
+    this.authSubscription = this.authService.getCurrentUser().subscribe(res => {
       if (res) {
         this.isAuth = !!res.uid;
       }
@@ -23,11 +26,20 @@ export class HeaderComponent implements OnInit {
         this.isAuth = false;
       }
     })
+    this.isVisible = false;
+  }
+
+  ngOnDestroy() {
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe()
+    }
   }
 
   logout() {
     this.authService.logout().then(() => {
       this.router.navigate(['/login'])
     })
+    this.isVisible = false;
   }
+
 }
